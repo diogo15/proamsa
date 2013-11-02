49c2bed39a7a737484b8e1c0240de1faceaf8f42
diogo15 <badboy2>
2013-11-02 17:41:27 -0600

    Main.js Added, functions.php clean up..

--- proamsa_theme/functions.php
+++ proamsa_theme/functions.php
@@ -25,21 +25,10 @@ function proamsa_theme_setup() {
 	 */
 	load_theme_textdomain( 'proamsa_theme', get_template_directory() . '/languages' );
 
-	/**
-	 * Add default posts and comments RSS feed links to head
-	 */
 	add_theme_support( 'automatic-feed-links' );
 
-	/**
-	 * Enable support for Post Thumbnails on posts and pages
-	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-	 */
 	//add_theme_support( 'post-thumbnails' );
 
-	/**
-	 * This theme uses wp_nav_menu() in one location.
-	 */
 	register_nav_menus( array(
 		'primary' => __( 'Primary Menu', 'proamsa_theme' ),
 	) );
@@ -81,24 +70,20 @@ add_action( 'widgets_init', 'proamsa_theme_widgets_init' );
 function proamsa_theme_scripts() {
 	wp_enqueue_style( 'proamsa_theme-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'proamsa_theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'proamsa_theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '1', true );
+	
+	wp_enqueue_script( 'proamsa-main-JS', get_template_directory_uri() . '/js/main-proamsa.js', array(), '1', true );
+	
+	
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
-	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'proamsa_theme-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-	}
+	
 }
 add_action( 'wp_enqueue_scripts', 'proamsa_theme_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
-//require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
@@ -115,7 +100,4 @@ require get_template_directory() . '/inc/extras.php';
  */
 require get_template_directory() . '/inc/customizer.php';
 
-/**
- * Load Jetpack compatibility file.
- */
-require get_template_directory() . '/inc/jetpack.php';
+
