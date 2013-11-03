6ae2dbc7221d8dcd8090a817d366192868aca157
unoscar <evoscar@gmail.com>
2013-11-02 20:18:05 -0600

    Estilos añadidos y html del header añadido

--- proamsa_theme/header.php
+++ proamsa_theme/header.php
@@ -9,13 +9,12 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 
-<?php wp_head(); ?>
 
+<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -23,12 +22,33 @@
 		<div class="wraper">
     	<div class="main-header">
         	<div class="contact-logos">
+            	<span class="icon"><img src="../imagenes/icono-telefono.png" width="37" height="38" alt=""/>
+                	<span class="arrow-bg"><span>xxx</span></span>
+                </span>
+                
+                <span class="icon"><img src="../imagenes/icono-correo.png" width="38" height="38" alt=""/>
+                <span class="arrow-bg"><span>info@grupoproamsa.com</span></span>
+                </span>
             </div>
             <div class="logo">
+            	<img src="../imagenes/logo.png" width="252" height="176" alt=""/>
             </div>
             <div class="nav">
-            	<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+              	<?php //wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+            	<ul>
+                	<li><a href="#">Inicio</a></li>
+                    <li class="dos-lineas"><a href="#">Quienes Somos</a></li>
+                    <li><a href="#">Servicios</a></li>
+                    <li><a href="#">Noticias</a></li>
+                    <li class="largo"><a href="#">Descargas</a></li>
+                    <li><a href="#">Galería</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
             </div>
+            <div class="clear"></div>
         </div>
     	<div class ="main-content">
        		<div class="inner">
+          
+            
+        
