7f810080a8533213bf502b95a8d20e7976f7c0d6
unoscar <evoscar@gmail.com>
2013-11-02 21:28:33 -0600

    Se introdujo todos los contenidos del header y imagenes de fondo v2

--- proamsa_theme/functions.php
+++ proamsa_theme/functions.php
@@ -5,7 +5,9 @@
  * @package proamsa_theme
  */
 
-
+define( 'THEME_DIR', get_bloginfo('template_url'));
+define( 'IMG_DIR', THEME_DIR ."/img");
+define( 'JS_DIR', THEME_DIR ."/js");
 
 if ( ! function_exists( 'proamsa_theme_setup' ) ) :
 /**
@@ -69,13 +71,15 @@ add_action( 'widgets_init', 'proamsa_theme_widgets_init' );
  */
 function proamsa_theme_scripts() {
 	wp_enqueue_style( 'proamsa_theme-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '1', true );
+	wp_enqueue_style( 'supersized-jquery-style', THEME_DIR . '/css/supersized.css' );
+	wp_enqueue_style( 'oswald-googlefont', 'http://fonts.googleapis.com/css?family=Oswald:300,400' );
 	
-	wp_enqueue_script( 'proamsa-main-JS', get_template_directory_uri() . '/js/main-proamsa.js', array(), '1', true );
+	wp_enqueue_script( 'jquery');
+	wp_enqueue_script( 'skip-link-focus-fix', JS_DIR . '/skip-link-focus-fix.js', array(), '1' );
+	wp_enqueue_script( 'proamsa-main-JS', JS_DIR . '/main-proamsa.js', array(), '1'  );
+	wp_enqueue_script( 'supersized-jquery-JS', JS_DIR . '/supersized.3.2.7.min.js', array(), '1' );
 	
 	
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
