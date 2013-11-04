c7cdc557be5d358070660e6240044588f7c6d2b7
unoscar <evoscar@gmail.com>
2013-11-04 10:55:00 -0600

    added footer content, images and styles

--- proamsa_theme/footer.php
+++ proamsa_theme/footer.php
@@ -11,13 +11,32 @@
 		</div>
         </div>
         <div class="footer-logos">
-       
+       		<div class="logo-1"><img src="<?php echo IMG_DIR; ?>/footer-logo-1.png" width="312" height="103" alt=""/></div>
+            <div class="logo-2"><img src="<?php echo IMG_DIR; ?>/footer-logo-2.png" width="312" height="103" alt=""/></div>
+            <div class="logo-3"><img src="<?php echo IMG_DIR; ?>/footer-logo-3.png" width="312" height="103" alt=""/></div>
+            <div class="clear"></div>
         </div>
          <div class="footer-contact">
-       
+       			<div class="contact-logo">
+                	<img src="<?php echo IMG_DIR; ?>/footer-contact-logo.png" width="89" height="62" alt=""/>
+                </div>
+            <p>GRUPO PROAMSA nace en el año 1999 con la fundación de la Compañía Proyectos Ambientales PROAMSA S.A., la cual surge con el objetivo principal de brindar consultorías en el campo de la Ingeniería Sanitaria y Ambiental. En el año 2002 se constituye también la empresa CPA Constructora Proyectos Ambientales S.A., con el fin de ampliar nuestros servicios a la construcción de obras civiles, tales como proyectos “llave en mano” de sistemas de tratamiento de aguas residuales. </p>
+            <a href="#" ><img class="facebook" src="<?php echo IMG_DIR; ?>/facebook-logo.png" width="32" height="31" alt=""/></a>
+            	<div class="contact-us">
+                	<img class="arrow-right" src="<?php echo IMG_DIR; ?>/arrow-right.png" width="14" height="27" alt=""/>
+                    <p> <span>CONTACTENOS</span><br/>
+               		CRC +(506) 2283-0876<br/>
+                       DOM +(809) 983-6095</p>
+                    <img class="arrow-left" src="<?php echo IMG_DIR; ?>/arrow-left.png" width="14" height="27" alt=""/>
+                    <div class="clear"></div>
+                </div>
+           		<div class="clear"></div>
         </div>
          <div class="footer-legal">
-       
+       		<p class="copy">&copy; 2013 Todos los derechos reservados, Proamsa<br/>
+Teléfonos: CRC +(506) 2283-0876  DOM +(809) 986095  info@grupoproamsa.com</p>
+            <p class="credit">Diseñado y Desarrollado por <img src="<?php echo IMG_DIR; ?>/dame-click-logo.png" alt=""/> DameClic</p>
+            <div class="clear"></div>
         </div>
     </div>
 <?php wp_footer(); ?>
