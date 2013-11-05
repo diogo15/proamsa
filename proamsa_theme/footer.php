025492029881a112ea21e7b21e74b361ceb5dbb9
unoscar <evoscar@gmail.com>
2013-11-04 20:44:40 -0600

    changes in footer

--- proamsa_theme/footer.php
+++ proamsa_theme/footer.php
@@ -20,9 +20,11 @@
        			<div class="contact-logo">
                 	<img src="<?php echo IMG_DIR; ?>/footer-contact-logo.png" width="89" height="62" alt=""/>
                 </div>
-            <p>GRUPO PROAMSA nace en el año 1999 con la fundación de la Compañía Proyectos Ambientales PROAMSA S.A., la cual surge con el objetivo principal de brindar consultorías en el campo de la Ingeniería Sanitaria y Ambiental. En el año 2002 se constituye también la empresa CPA Constructora Proyectos Ambientales S.A., con el fin de ampliar nuestros servicios a la construcción de obras civiles, tales como proyectos “llave en mano” de sistemas de tratamiento de aguas residuales. </p>
+                <div class="company-info">
+           <?php echo get_box('informacion-compania-pie-de-pagina'); ?>
+           		</div>
             <a href="#" ><img class="facebook" src="<?php echo IMG_DIR; ?>/facebook-logo.png" width="32" height="31" alt=""/></a>
-            	<div class="contact-us">
+            	<div class="contactenos">
                 	<img class="arrow-right" src="<?php echo IMG_DIR; ?>/arrow-right.png" width="14" height="27" alt=""/>
                     <p> <span>CONTACTENOS</span><br/>
                		CRC +(506) 2283-0876<br/>
